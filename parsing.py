5cbe1eb12ff8ea60decef258dd7c183a88cc3bd4
thimma <thimmarayan.krishnappa@gmail.com>
2018-01-14 21:41:26 +0530

    Jan 14

--- parsing.py
+++ parsing.py
@@ -1,14 +1,84 @@
-import optparse
+# import re 
 
-#usage = 'usage: %prog'
-options=optparse.OptionParser('[-a], [-s],[-m],[-d]')
-options.add_option('-a',action='store_true',dest='add',default=False,help='Addition the two given value')
-options.add_option('-s',action='store_true',dest='sub',default=False,help='subtract the two given value')
-options.add_option('-m',action='store_true',dest='mul',default=False,help='Multiply the two given value')
-options.add_option('-d',action='store_true',dest='div',default=False,help='Divide the two given value')
-(option,args)=options.parse_args()
-print(option.add)
-if option.add:
-    print('add value',args,)
-elif option.sub:
-    print("subtract value")
+# text_search ='''
+# abcdefghifslkfjadfklj
+# ABCDAFDGEARAWEGGAFASDF
+# 1243412798571934719278341
+# thimma.com
+# thimma@gmail.com
+# thimmrayan@hpe.com
+# thimm.rayan@hpe.com
+# thimma-rayan@hpe.com
+# kumar_krish@gmail.co.in
+# thimma-rayan@hpe-co.in 
+# https://www.thimmarayan.com
+# http://www.gmail.com 
+# http://thimmarayan.com 
+# https://youtube.com 
+# http://www.gmail.co.in
+# http://www.gmail-co.in
+# Ha HaHa
+# 234-424-2145
+# 765*876-4321
+# 456.908.6574
+# 800-876-4321
+# 900.908.6574
+# Mr. Scrahf
+# Ms Davis
+# Mrs. Robinson
+# Mr. T
+# Mr Smith
+# mat 
+# bat
+# cat 
+# pat
+# '''
+
+# fake_name='''
+# Dave Martin
+# 643-555-7654
+# 173 Main St., Springfield RI 78907
+# davemarting@bogusemail.com
+# Charles Harris
+# 800-555-5668
+# 987 High St., Alantis VA 34075
+# charlesharris@bogusemail.com
+# '''
+# urls='''
+# https://www.thimmarayan.com
+# http://www.gmail.com 
+# http://thimmarayan.com 
+# https://youtube.com 
+# http://www.gmail.co.in
+# '''
+
+# sentence = 'Start a sentence and then bring it to an end'
+
+from threading import Thread
+from time import sleep
+
+
+class myThread(Thread):
+
+    def __init__(self, val):
+        Thread.__init__(self)
+        self.val = val
+
+    def run(self):
+        for i in range(self.val):
+            self.myfunc(i)
+        
+        print(self.getName())
+
+
+    def myfunc(self,v):
+        sleep(v)
+        print('i am called from thread run',v)
+    
+
+if __name__ == '__main__':
+    t = myThread(9)
+    t.setName('Thread1')
+    t.start()
+    t.join()
+
