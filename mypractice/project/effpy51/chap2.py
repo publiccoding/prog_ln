9c3210d177000d0a05cc9a0f1f281cebb8785adb
Thimmarayan krishnappa <thimmarayan.krishnappa@gmail.com>
2018-10-18 06:34:41 +0530

    new chanes on 18th october

--- mypractice/project/effpy51/chap2.py
+++ mypractice/project/effpy51/chap2.py
@@ -1,3 +1,5 @@
+import datetime
+import time
 # 14. Prefer Exceptions to Returning None
 # Raise	exceptions to indicate special situations instead of returning None. Expect	thecalling code to handle exceptions properly when they are documented.
 
@@ -28,18 +30,119 @@
 # print(add(15))
 
 
-def sort_values(values, groups):
+# def sort_values(values, groups):
 	
-	def helper(x):
-		if x in groups:
-			print((0,x))
-			return (1,x)
-		print((1,x))
-		return (0,x)
-	values.sort(key=helper)
+# 	def helper(x):
+# 		if x in groups:
+# 			print((0,x))
+# 			return (1,x)
+# 		print((1,x))
+# 		return (0,x)
+# 	values.sort(key=helper)
 
-numbers	=	[8,	3,	1,	2,	5,	4,	7,	6]
-group	=	{2,	3,	5,	7}
+# numbers	=	[8,	3,	1,	2,	5,	4,	7,	6]
+# group	=	{2,	3,	5,	7}
 
-sort_values(numbers,group)
-print(numbers)
+# sort_values(numbers,group)
+# print(numbers)
+
+
+#16. Consider Generators Instead of	Returning Lists
+
+
+# def index_file(handle):
+# 	offset = 0
+# 	for line in handle:
+# 		if line:
+# 			print(offset)
+# 			yield offset
+# 		for word in line:
+# 			offset += 1
+# 			print(offset)
+# 			if word == ' ':
+# 				yield offset
+
+# with open('/root/prog_ln/mypractice/project/file1.txt','w') as f:
+# 	it= index_file(f)
+# 	result = (it, 0, 3)
+# 	for val in result:
+# 		print(val)
+
+#17. Be	Defensive When Iterating Over Arguments
+# Always pass as container to the function 
+
+# def normalize_func(get_iter):
+# 	total = sum(get_iter())
+# 	result = []
+
+# 	for val in get_iter():
+# 		percent = 100 * value/total 
+# 		result.appen(percent)
+# 	return result
+
+
+# class ReadVisits(object):
+	
+
+# 	def __init__(self, data_path):
+# 		self.data_path = data_path
+
+# 	def __iter__(self):
+# 		with open(self.data_path) as file:
+# 			for line in file:
+# 				yield line
+
+# visits = ReadVisits(path)
+# percent = normalize_func(visits)
+# print(percent)
+
+#18. Reduce	Visual Noise with Variable Positional Arguments
+
+# def log(seq, msg, *val):
+# 	if not val:
+# 		print('%s: %s'%(seq, msg))
+# 	else:
+# 		value_str = ', '.join(str(x) for x in val)
+# 		print('%s%s%s'%(seq, msg, value_str))
+
+# log(1, 'Favorites', 7, 33)
+# log('Favorite numbers',	7, 33)
+
+
+#19. Provide Optional Behavior with Keyword Arguments
+
+# def	flow_rate(weight_diff, time_diff):
+# 	return	weight_diff	/	time_diff
+# weight_diff	=	0.5
+# time_diff	=	3
+# flow = flow_rate(weight_diff, time_diff)
+# print('%.3f	kg	per	second' % flow)
+
+#20. Use None and Docstrings to	Specify	Dynamic	Default	Arguments
+"""
+# Things	to	Remember
+# Default arguments	are	only evaluated once  during	function definition	at module load time. This can cause odd behaviors for dynamic values (like {} or []).  
+# Use None as the default value	for	keyword	arguments that have	a dynamic value. Document the actual default behavior in the functions	docstring.
+"""
+def	log(message,	when=None):
+	"""Log	a	message	with	a	timestamp.
+	Args:
+		message: Message to	print.
+		when: datetime of when the message occurred.
+		Defaults to	the	present	time.
+	"""
+	when = datetime.date.today() if when is None else when
+	print('%s:	%s'	% (message, when))
+
+# log('Hi	there!')
+# time.sleep(0.1)
+# log('Hi	again!')
+
+#21. Enforce Clarity with Keyword-Only Arguments
+
+# Things	to	Remember
+# 	Keyword	arguments make the intention of	a function	call more clear.
+# 	Use	keyword-only arguments to force callers to supply keyword arguments for
+#   potentially	confusing functions, especially	those that accept multiple Boolean flags.
+# 	Python 3 supports explicit syntax	for	keyword-only arguments in functions.
+# 	Python 2 can emulate keyword-only arguments for functions by using **kwargs and	manually raising TypeError exceptions.
--- mypractice/project/effpy51/test.py
+++ mypractice/project/effpy51/test.py
@@ -1,4 +1,38 @@
-numbers = [0, 1, 2, 3, 11, 4, 5, 6, 89, 7, 8, 9]
-print(numbers)
+# import difflib
+# import sys
+# import collections
+# import operator 
 
-print(numbers.sort(key={1:8,0:3}))
+# total = 1000
+
+# with open('/root/prog_ln/mypractice/project/file1.txt','w') as f:
+# 	for x in range(total):
+# 		f.write('Line no {}\n'.format(x))
+
+# with open('/root/prog_ln/mypractice/project/file2.txt','w') as f:
+# 	for x in range(total + 10):
+# 		if x in (500,505,total- 2):
+# 			continue
+# 		f.write('Line no {}\n'.format(x))
+
+# with open('/root/prog_ln/mypractice/project/file3.txt','w') as f:
+# 	for x in range(1002,1005):
+# 		f.write('Line no {}\n'.format(x))
+
+
+# with open('/root/prog_ln/mypractice/project/file1.txt','r') as f1 , open('/root/prog_ln/mypractice/project/file2.txt','r') as f2, open('/root/prog_ln/mypractice/project/file3.txt','r') as f3:
+# 	from_list = f1.readlines()
+# 	to_list = f2.readlines()
+# 	dis_list = f3.readlines() # disable data which need to be subtracted from the first file
+# 	to_list = [a - b for a, b in zip(from_list, to_list)]
+# 	#to_list = list(map(operator.sub, to_list, dis_list))
+# 	#to_list = [item for item in to_list if item not in dis_list]
+# 	diff = difflib.ndiff(from_list, to_list)
+# 	for line in diff:
+# 		line = line.strip()
+# 		if line.startswith('-'):
+# 			print(line)
+# 		elif line.startswith('+'):
+# 			print(line)
+
+
